c96c041a4917a0aa163a19cbebf283bfa2e3e708
Arseniy Krasnov <arseniy@krasnoff.org>
2020-07-22 10:46:55 +0300

    Initial

---
+++ data.js
@@ -0,0 +1,557 @@
+var data = [
+    [
+        //pVAX-VEGF-p2A-FGF2-p2A-dsRed
+        {
+            "axis": "IL-1b",
+            "value": 1.2475
+        },
+        {
+            "axis": "IL-1ra",
+            "value": 31.9125
+        },
+        {
+            "axis": "IL-2",
+            "value": 3.345
+        },
+        {
+            "axis": "IL-4",
+            "value": 1.5075
+        },
+        {
+            "axis": "IL-5",
+            "value": 5.495
+        },
+        {
+            "axis": "IL-6",
+            "value": 4.665
+        },
+        {
+            "axis": "IL-7",
+            "value": 6.3725
+        },
+        {
+            "axis": "IL-8",
+            "value": 64.385
+        },
+        {
+            "axis": "IL-9",
+            "value": 7.89
+        },
+        {
+            "axis": "IL-10",
+            "value": 12.2225
+        },
+        {
+            "axis": "IL-12(p70)",
+            "value": 14.12
+        },
+        {
+            "axis": "IL-13",
+            "value": 6.5575
+        },
+        {
+            "axis": "IL-15",
+            "value": 12.5075
+        },
+        {
+            "axis": "IL-17",
+            "value": 12.5075
+        },
+        {
+            "axis": "Eotaxin",
+            "value": 8.2525
+        },
+        {
+            "axis": "FGF basic",
+            "value": 44.9125
+        },
+        {
+            "axis": "G-CSF",
+            "value": 48.0125
+        },
+        {
+            "axis": "GM-CSF",
+            "value": 27.415
+        },
+        {
+            "axis": "IFN-g",
+            "value": 9.565
+        },
+        {
+            "axis": "Hu IP-10",
+            "value": 114.41
+        },
+        {
+            "axis": "MCP-1",
+            "value": 17.78
+        },
+        {
+            "axis": "MIP-1a",
+            "value": 1.0975
+        },
+        {
+            "axis": "PDGF-bb",
+            "value": 17.075
+        },
+        {
+            "axis": "MIP-1b",
+            "value": 2.2825
+        },
+        {
+            "axis": "RANTES",
+            "value": 7.3425
+        },
+        {
+            "axis": "TNF-a",
+            "value": 9.1975
+        },
+        {
+            "axis": "VEGF",
+            "value": 440.89
+        }
+    ],
+    [
+        //pVAX-VEGF-p2A-dsRed
+        {
+            "axis": "IL-1b",
+            "value": 1.4625
+        },
+        {
+            "axis": "IL-1ra",
+            "value": 27.8875
+        },
+        {
+            "axis": "IL-2",
+            "value": 34.6925
+        },
+        {
+            "axis": "IL-4",
+            "value": 1.6125
+        },
+        {
+            "axis": "IL-5",
+            "value": 9.105
+        },
+        {
+            "axis": "IL-6",
+            "value": 6.6675
+        },
+        {
+            "axis": "IL-7",
+            "value": 10.3175
+        },
+        {
+            "axis": "IL-8",
+            "value": 62.2375
+        },
+        {
+            "axis": "IL-9",
+            "value": 8.9725
+        },
+        {
+            "axis": "IL-10",
+            "value": 10.2475
+        },
+        {
+            "axis": "IL-12(p70)",
+            "value": 18.0125
+        },
+        {
+            "axis": "IL-13",
+            "value": 32.6575
+        },
+        {
+            "axis": "IL-15",
+            "value": 21.565
+        },
+        {
+            "axis": "IL-17",
+            "value": 79.315
+        },
+        {
+            "axis": "Eotaxin",
+            "value": 64.575
+        },
+        {
+            "axis": "FGF basic",
+            "value": 15.2725
+        },
+        {
+            "axis": "G-CSF",
+            "value": 35.97
+        },
+        {
+            "axis": "GM-CSF",
+            "value": 28.2825
+        },
+        {
+            "axis": "IFN-g",
+            "value": 8.8425
+        },
+        {
+            "axis": "Hu IP-10",
+            "value": 119.7425
+        },
+        {
+            "axis": "MCP-1",
+            "value": 12.2775
+        },
+        {
+            "axis": "MIP-1a",
+            "value": 3.9625
+        },
+        {
+            "axis": "PDGF-bb",
+            "value": 12.2625
+        },
+        {
+            "axis": "MIP-1b",
+            "value": 2.0525
+        },
+        {
+            "axis": "RANTES",
+            "value": 9.9175
+        },
+        {
+            "axis": "TNF-a",
+            "value": 14.9875
+        },
+        {
+            "axis": "VEGF",
+            "value": 1227.865
+        }
+    ],
+    [
+        //pVAX-FGF2-p2A-dsRed
+        {
+            "axis": "IL-1b",
+            "value": 0.6475
+        },
+        {
+            "axis": "IL-1ra",
+            "value": 8.0125
+        },
+        {
+            "axis": "IL-2",
+            "value": 1.1725
+        },
+        {
+            "axis": "IL-4",
+            "value": 0.6075
+        },
+        {
+            "axis": "IL-5",
+            "value": 4.1825
+        },
+        {
+            "axis": "IL-6",
+            "value": 0.655
+        },
+        {
+            "axis": "IL-7",
+            "value": 3.4075
+        },
+        {
+            "axis": "IL-8",
+            "value": 53.1225
+        },
+        {
+            "axis": "IL-9",
+            "value": 4.75
+        },
+        {
+            "axis": "IL-10",
+            "value": 2.9275
+        },
+        {
+            "axis": "IL-12(p70)",
+            "value": 13.45
+        },
+        {
+            "axis": "IL-13",
+            "value": 1.7025
+        },
+        {
+            "axis": "IL-15",
+            "value": 12.5075
+        },
+        {
+            "axis": "IL-17",
+            "value": 5.3025
+        },
+        {
+            "axis": "Eotaxin",
+            "value": 1.6425
+        },
+        {
+            "axis": "FGF basic",
+            "value": 152.2475
+        },
+        {
+            "axis": "G-CSF",
+            "value": 14.7
+        },
+        {
+            "axis": "GM-CSF",
+            "value": 7.6975
+        },
+        {
+            "axis": "IFN-g",
+            "value": 5.4375
+        },
+        {
+            "axis": "Hu IP-10",
+            "value": 37.8875
+        },
+        {
+            "axis": "MCP-1",
+            "value": 11.26
+        },
+        {
+            "axis": "MIP-1a",
+            "value": 0.3075
+        },
+        {
+            "axis": "PDGF-bb",
+            "value": 12.47
+        },
+        {
+            "axis": "MIP-1b",
+            "value": 1.4125
+        },
+        {
+            "axis": "RANTES",
+            "value": 3.3825
+        },
+        {
+            "axis": "TNF-a",
+            "value": 1.8325
+        },
+        {
+            "axis": "VEGF",
+            "value": 59.1225
+        }
+    ],
+    [
+        //pVAX-dsRed
+        {
+            "axis": "IL-1b",
+            "value": 0.67
+        },
+        {
+            "axis": "IL-1ra",
+            "value": 6.2325
+        },
+        {
+            "axis": "IL-2",
+            "value": 1.205
+        },
+        {
+            "axis": "IL-4",
+            "value": 0.1925
+        },
+        {
+            "axis": "IL-5",
+            "value": 4.165
+        },
+        {
+            "axis": "IL-6",
+            "value": 0.7125
+        },
+        {
+            "axis": "IL-7",
+            "value": 3.5825
+        },
+        {
+            "axis": "IL-8",
+            "value": 42.3
+        },
+        {
+            "axis": "IL-9",
+            "value": 4.5
+        },
+        {
+            "axis": "IL-10",
+            "value": 2.5375
+        },
+        {
+            "axis": "IL-12(p70)",
+            "value": 8.7175
+        },
+        {
+            "axis": "IL-13",
+            "value": 1.5825
+        },
+        {
+            "axis": "IL-15",
+            "value": 12.5525
+        },
+        {
+            "axis": "IL-17",
+            "value": 5.1675
+        },
+        {
+            "axis": "Eotaxin",
+            "value": 1.525
+        },
+        {
+            "axis": "FGF basic",
+            "value": 12.83
+        },
+        {
+            "axis": "G-CSF",
+            "value": 14.7225
+        },
+        {
+            "axis": "GM-CSF",
+            "value": 6.445
+        },
+        {
+            "axis": "IFN-g",
+            "value": 4.435
+        },
+        {
+            "axis": "Hu IP-10",
+            "value": 59.995
+        },
+        {
+            "axis": "MCP-1",
+            "value": 7.5225
+        },
+        {
+            "axis": "MIP-1a",
+            "value": 0.195
+        },
+        {
+            "axis": "PDGF-bb",
+            "value": 5.71
+        },
+        {
+            "axis": "MIP-1b",
+            "value": 1.145
+        },
+        {
+            "axis": "RANTES",
+            "value": 4.365
+        },
+        {
+            "axis": "TNF-a",
+            "value": 1.75
+        },
+        {
+            "axis": "VEGF",
+            "value": 60.7625
+        }
+    ],
+    [
+        //NTC
+        {
+            "axis": "IL-1b",
+            "value": 0.573333333333333
+        },
+        {
+            "axis": "IL-1ra",
+            "value": 5.36333333333333
+        },
+        {
+            "axis": "IL-2",
+            "value": 0.796666666666667
+        },
+        {
+            "axis": "IL-4",
+            "value": 0.433333333333333
+        },
+        {
+            "axis": "IL-5",
+            "value": 3.21333333333333
+        },
+        {
+            "axis": "IL-6",
+            "value": 0.896666666666667
+        },
+        {
+            "axis": "IL-7",
+            "value": 2.27333333333333
+        },
+        {
+            "axis": "IL-8",
+            "value": 36.9766666666667
+        },
+        {
+            "axis": "IL-9",
+            "value": 3.34666666666667
+        },
+        {
+            "axis": "IL-10",
+            "value": 2.65
+        },
+        {
+            "axis": "IL-12(p70)",
+            "value": 8.39666666666667
+        },
+        {
+            "axis": "IL-13",
+            "value": 1.53666666666667
+        },
+        {
+            "axis": "IL-15",
+            "value": 8.37333333333333
+        },
+        {
+            "axis": "IL-17",
+            "value": 5.70333333333333
+        },
+        {
+            "axis": "Eotaxin",
+            "value": 1.94
+        },
+        {
+            "axis": "FGF basic",
+            "value": 18.06
+        },
+        {
+            "axis": "G-CSF",
+            "value": 10.02
+        },
+        {
+            "axis": "GM-CSF",
+            "value": 6.98333333333333
+        },
+        {
+            "axis": "IFN-g",
+            "value": 6.95333333333333
+        },
+        {
+            "axis": "Hu IP-10",
+            "value": 77.0433333333333
+        },
+        {
+            "axis": "MCP-1",
+            "value": 4.38666666666667
+        },
+        {
+            "axis": "MIP-1a",
+            "value": 0.256666666666667
+        },
+        {
+            "axis": "PDGF-bb",
+            "value": 15.61
+        },
+        {
+            "axis": "MIP-1b",
+            "value": 1.26666666666667
+        },
+        {
+            "axis": "RANTES",
+            "value": 4.07333333333333
+        },
+        {
+            "axis": "TNF-a",
+            "value": 1.45
+        },
+        {
+            "axis": "VEGF",
+            "value": 63.3033333333333
+        }
+    ]
+];
